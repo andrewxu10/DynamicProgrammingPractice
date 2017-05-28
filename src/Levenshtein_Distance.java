66430303452091c4fcc4e1c46820ae11ee820340
Andrew Xu <andrewxu_2016@depauw.edu>
2017-05-28 16:57:34 -0700

    debugged levenshtein distance algo. next: add memoization, dp, and iterative solutions

--- src/Levenshtein_Distance.java
+++ src/Levenshtein_Distance.java
@@ -2,19 +2,18 @@
 public class Levenshtein_Distance {
 	
 	public static int Levenshtein(String string1, String string2) {
+		int cost = 0;
 		if(string1.length() == 0) {
 			return string2.length();
 		}
 		if(string2.length() == 0) {
-			return string2.length();
+			return string1.length();
 		}
-		int cost = 0;
 //		System.out.println(string1.substring(0, string1.length() - 1));
 //		System.out.println(string2.substring(0, string2.length() - 1));
 //		System.out.println(string2.charAt(string2.length() - 1));
 //		System.out.println(string2.length() - 1);
 		if(string1.charAt(string1.length() - 1) == string2.charAt(string2.length() - 1)){
-		//if(string1.charAt(string1.length() - 1) == string2.charAt(string2.length()) - 1) {
 			cost = 0; //last char = match
 		} else {
 			cost = 1; //last char needs to be swapped
@@ -27,13 +26,13 @@ public class Levenshtein_Distance {
 						//2: remove last char from string1 && return int + 1 (add)
 						Levenshtein(string1.substring(0, string1.length() - 1), string2) + 1,
 						//2: remove last char from string2 && return int + 1 (subtract)
-						Levenshtein(string2, string2.substring(0, string2.length() - 1)) + 1
+						Levenshtein(string1, string2.substring(0, string2.length() - 1)) + 1
 						)
 				);
 	}
 	
 	public static void main(String[] args) {
 		//System.out.println("billy".charAt("billy".length() - 1));
-		System.out.println(Levenshtein("billy", "bill"));
+		System.out.println(Levenshtein("mzzzm", "mmmmm"));
 	}
 }
