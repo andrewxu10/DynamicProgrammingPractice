9325a3dd98033e6d7f32b12c1b520c96832af25e
Andrew Xu <andrewxu_2016@depauw.edu>
2017-05-29 22:56:22 -0700

    word break recursive progress

--- src/Word_Break.java
+++ src/Word_Break.java
@@ -3,20 +3,31 @@ import java.util.Arrays;
 public class Word_Break {
 
 	public static void main (String[] args) {
-		String[] strDict = new String[]{"apple", "pie", "applepie"};
-		wordBreak("applepie", strDict);
+		String[] strDict = new String[]{"apple", "pie", "albino", "juice", "ass", "assan", "an"};
+		wordBreak("assan", strDict);
 		
 	}
 	
 	public static void wordBreak(String string, String[] strDict) {
 		if (Arrays.asList(strDict).contains(string)) {
 			System.out.println(string);
+			
 		}
-		for(int i = 0; i < string.length(); i++) {
+		
+		for(int i = 1; i < string.length(); i++) {
 			String a = string.substring(0, i);
 			String b = string.substring(i, string.length());
+//			System.out.println(a);
+//			System.out.println(b);
+//			if(wordBreak(a, strDict) && wordBreak(b, strDict)) {
+//				System.out.println(a);
+//				System.out.println(b);
+//				System.out.println();
+//				
+//				return true;
+			
 			wordBreak(a, strDict);
 			wordBreak(b, strDict);
-		}
+			}
 	}
 }
