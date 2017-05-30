fb90ea326843333f022a424716c1d46e30b7792e
Andrew Xu <andrewxu_2016@depauw.edu>
2017-05-30 00:00:32 -0700

    better recursive wordBreak

--- src/Word_Break.java
+++ src/Word_Break.java
@@ -4,30 +4,43 @@ public class Word_Break {
 
 	public static void main (String[] args) {
 		String[] strDict = new String[]{"apple", "pie", "albino", "juice", "ass", "assan", "an"};
-		wordBreak("assan", strDict);
-		
+		wordBreak("applepiealbinojuiceassan", strDict);	
 	}
 	
-	public static void wordBreak(String string, String[] strDict) {
-		if (Arrays.asList(strDict).contains(string)) {
+//	public static boolean wordBreak(String string, String[] strDict) {
+//		if (Arrays.asList(strDict).contains(string)) {
+//			System.out.println(string);
+//		}
+//		
+//		for(int i = 1; i < string.length(); i++) {
+//			String a = string.substring(0, i);
+//			String b = string.substring(i, string.length());
+//			if(wordBreak(a, strDict) && wordBreak(b, strDict)) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+	
+	public static boolean wordBreak(String string, String[] strDict) {
+		boolean flag = false;
+		if(Arrays.asList(strDict).contains(string)) {
 			System.out.println(string);
-			
+			flag = true;
 		}
-		
-		for(int i = 1; i < string.length(); i++) {
-			String a = string.substring(0, i);
+		for(int i=1; i<string.length(); i++) {
+			String a = string.substring(0,i);
 			String b = string.substring(i, string.length());
 //			System.out.println(a);
 //			System.out.println(b);
-//			if(wordBreak(a, strDict) && wordBreak(b, strDict)) {
-//				System.out.println(a);
-//				System.out.println(b);
-//				System.out.println();
-//				
-//				return true;
-			
-			wordBreak(a, strDict);
-			wordBreak(b, strDict);
+//			System.out.println();
+			if(Arrays.asList(strDict).contains(a)) {
+				if(wordBreak(b, strDict)) {
+					System.out.println(a);
+					return true;
+				}
 			}
+		}
+		return flag;
 	}
 }
