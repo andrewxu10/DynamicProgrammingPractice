717805e1a4090f8caa42c8583b33c09ccdb9e33d
Andrew Xu <andrewxu_2016@depauw.edu>
2017-06-02 16:41:11 -0700

    lvl 9k stress test added lol. crying because it doesn't work

--- src/Word_Break.java
+++ src/Word_Break.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class Word_Break {
 
 	public static void main (String[] args) {
-		//String[] strDict = new String[]{"apple", "pie", "albino", "juice", "ass", "assan", "an"};
 		HashMap<String, String> strDict = new HashMap<String, String>();
         strDict.put("assan", "assan");
         strDict.put("assanan", "assanan");
@@ -18,74 +17,17 @@ public class Word_Break {
         strDict.put("juice", "juice");
         //wordBreakHandler("applepiealbinojuiceassan", strDict); //stress test
 		wordBreakHandler("applepiealbinojuiceassanan", strDict); //ultimate stress test
-
-		
+		wordBreakHandler("applepiealbinojuiceassanan", strDict); //now this is what i call a stress test - not working
 	}
-	
-//	public static boolean wordBreak(String string, String[] strDict) {
-//		if (Arrays.asList(strDict).contains(string)) {
-//			System.out.println(string);
-//		}
-//		
-//		for(int i = 1; i < string.length(); i++) {
-//			String a = string.substring(0, i);
-//			String b = string.substring(i, string.length());
-//			if(wordBreak(a, strDict) && wordBreak(b, strDict)) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-	
-	
-	
-//	public static boolean wordBreak(String string, String[] strDict) {
-//		boolean flag = false;
-//		if(Arrays.asList(strDict).contains(string)) {
-//			System.out.println(string);
-//			flag = true;
-//		}
-//		for(int i=1; i<string.length(); i++) {
-//			String a = string.substring(0,i);
-//			String b = string.substring(i, string.length());
-//			if(Arrays.asList(strDict).contains(a)) {
-//				if(wordBreak(b, strDict)) {
-//					System.out.println(a);
-//					return true;
-//				}
-//			}
-//		}
-//		return flag;
-//	}
-	
-	
-	
-//	public static void wordBreak(String string, String[] strDict, boolean recursed) {
-//		boolean present = false;
-//		boolean flush = false;
-//		if(Arrays.asList(strDict).contains(string) || string == "") {
-//			System.out.println(string);
-//			flush = true;
-//			if(string.length() > 1) {
-//				strDict.remove(string); //create .remove for strDict
-//				//if (wordBreak(string, modifiedStrDict).length() > 1) {
-//					//the zeroith field in the return array can be a flag to retry the entire recursion
-//					//if you find flag in array && recursed == false, 
-//					//redo the whole thing w/o the string in dict
-//			}
-//		}
 	
 	public static ArrayList<String> wordBreak(String string, HashMap<String, String> strDict, ArrayList<String> answers) {
 		if(string.length() == 0) {
-			return answers; //should be- add to list of answers
+			return answers; //to do: add to list of answers
 		}
 		if(string.length() > 1) {
-			if(findNextShortest(string, strDict) == null) { //if the last substring isn't flush with the string's suffix:
-				//for(int i = answers.size() - 1; i >= 0; i--) { //everything in this loop is broken
-					//System.out.println(string);
-					//System.out.println("hit");
-					//System.out.println("answers before removal: " + answers);
-					String locallyUnmatched = string;
+			if(findNextShortest(string, strDict) == null) { //if we're in the middle of a non-answer, backtrack one word (remove from dictionary, answers list, and un-process input string) and re-try:
+					//no need for loop. Recurse with the full state in the parameters.
+					String locallyUnmatched = string; //unprocessed string that indicates we are in a non-answer
 					String toBePoppedLocally = answers.get(answers.size() - 1);
 					System.out.println("remaining string: " + locallyUnmatched + " has no dict match '" + toBePoppedLocally + "' was removed from answers, dict, readded to the string to process");
 					strDict.remove(toBePoppedLocally); //pop the last answer from the dict
@@ -108,21 +50,6 @@ public class Word_Break {
 		}
 		return answers;
 	}
-	
-	public static String findNextShortest(String string, HashMap<String, String> strDict) {
-		for (int i = 1; i <= string.length(); i++) {
-			if(strDict.containsKey(string.substring(0, i))) {
-				return string.substring(0,i);
-			}
-		}
-		return null;
-	}
-	
-	public static void printList(ArrayList<String> arrayList) {
-		System.out.println(Arrays.deepToString(arrayList.toArray()));
-	}
-	
-	//a null answers list should be empty!
 		
 	//handler : run wordBreak, then run wordBreak - last answer
 	public static void wordBreakHandler(String string, HashMap<String, String> strDict) {
@@ -152,9 +79,16 @@ public class Word_Break {
 		}
 	}
 	
-	
-	
-	
-	
-	
+	//2 helper functions below:
+	public static String findNextShortest(String string, HashMap<String, String> strDict) {
+		for (int i = 1; i <= string.length(); i++) {
+			if(strDict.containsKey(string.substring(0, i))) {
+				return string.substring(0,i);
+			}
+		}
+		return null;
+	}
+	public static void printList(ArrayList<String> arrayList) {
+		System.out.println(Arrays.deepToString(arrayList.toArray()));
+	}
 }
